b6fedca81fe27601a718b52171158fb6b5c18e3c
renosano <sato.siosio21@gmail.com>
2025-04-20 21:03:41 +0900

    描画ファイル追加

---
+++ ステップ関数.py
@@ -0,0 +1,10 @@
+import numpy as np
+# def step_function(x):
+#     if x > 0:
+#         return 1
+#     else:
+#         return 0
+
+def step_function(x):
+    y = x > 0
+    return y.astype(np.int)
